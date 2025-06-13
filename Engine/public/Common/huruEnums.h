812288f1e069e938363f6d8e1fec527d0a5cf82b
jaehuru <kimbs0516@gmail.com>
2025-06-14 05:44:14 +0900

    huruEnums.h architecture cleanup

--- Engine/public/Common/huruEnums.h
+++ Engine/public/Common/huruEnums.h
@@ -2,16 +2,6 @@
 
 #include <type_traits>
 
-namespace huru::utils
-{
-	template<typename T>
-	constexpr huru::enums::eLayerType ToEngineLayerType(T layerEnum)
-	{
-		static_assert(std::is_enum_v<T>, "Must be enum type.");
-		return static_cast<enums::eLayerType>(layerEnum);
-	}
-}
-
 namespace huru::enums
 {
 	enum class eComponentType
@@ -57,4 +47,14 @@ namespace huru::enums
 		HUD,
 		End,
 	};
+}
+
+namespace huru::utils
+{
+	template<typename T>
+	constexpr huru::enums::eLayerType ToEngineLayerType(T layerEnum)
+	{
+		static_assert(std::is_enum_v<T>, "Must be enum type.");
+		return static_cast<huru::enums::eLayerType>(layerEnum);
+	}
 }
